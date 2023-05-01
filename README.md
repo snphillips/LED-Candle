e18e6a0342f470803728b290d5772c436d4900f9
Sarah Phillips <snphillips@gmail.com>
2023-05-01 19:42:46 -0400

    replace trinket with itsybitsy & distance sensor

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # LED Proximity Sensing Flickering Candle
+
 An arduino led candle that flickers when something goes near it. This project is inspired by My [New Flame](https://www.ingo-maurer.com/en/products/my-new-flame/) by Moritz Waldemeyer for Ingo Maurer. Technically, this project is adapted from Phillip Burgess/paintyourdragon's [Animated Flame Pendant](https://learn.adafruit.com/animated-flame-pendant).
 
-
 ## Prerequisites
+
 - Read through Phillip Burgess' Animated Flame Pendant [Animated Flame Pendant docs](https://learn.adafruit.com/animated-flame-pendant).
-- Read through the [Ultrasonic Distance Sensor docs](https://learn.adafruit.com/ultrasonic-sonar-distance-sensors)
+- Review the [Adafruit Time of Flight Distance Sensor docs](https://learn.adafruit.com/adafruit-vl53l0x-micro-lidar-distance-sensor-breakout)
 - Have the [Arduino IDE](https://www.arduino.cc/en/software) installed on your computer
 - Have the parts listed in the Parts section below, plus wire, solder, flux & soldering iron
 - If you want to generate your own animations, have Python & FFmpeg installed on your computer (the leading multimedia framework): https://ffmpeg.org/
-
 
 ## How to Install Software onto Pro Trinket
 
@@ -16,34 +16,29 @@ First, install the [Arduino IDE](https://www.arduino.cc/en/software).
 
 - Clone this repo:
 
-
 `git clone https://github.com/snphillips/led-candle.git`
-
 
 - Connect the Pro Trinket into your computer. The instructions below assume you connected it using USB.
 
 - Using the Arduino IDE, open `led-candle.ino`.
 
 - Select the Pro Trinket 5V/16MHz (USB) or Pro Trinket 3V/12MHz (USB) board (depending on which version you have) from the Tools->Board menu.
-Next go into the Tools -> Programmer menu and select the USBtinyISP programmer.
+  Next go into the Tools -> Programmer menu and select the USBtinyISP programmer.
 
-- Upload `led-candle.ino` to the Pro Trinket board by pressing the tiny button on the Pro Trinket. Within eight seconds, select Sketch > Upload Using Programmer. Wait until you see the message "Done uploading". 
-If your code uploaded successfully, at the end of the output message you'll see a paragraph like this:
-
+- Upload `led-candle.ino` to the Pro Trinket board by pressing the tiny button on the Pro Trinket. Within eight seconds, select Sketch > Upload Using Programmer. Wait until you see the message "Done uploading".
+  If your code uploaded successfully, at the end of the output message you'll see a paragraph like this:
 
 `Sketch uses 21632 bytes (75%) of program storage space. Maximum is 28672 bytes.
 Global variables use 506 bytes of dynamic memory.`
 
-
 Nothing will happen yet, but now the code is on your Pro Trinket.
 
 ## How to Assemble Hardware
-The big picture is to follow the directions detailed in Phillip Burgess' Animated Flame Pendant docs, minus the section about 3D printing. Then, add in the ultrasonic distance sensor.
 
-TODO: flush out this section further
-
+TODO: flush out this section
 
 ## How to Generate Your Own Animations
+
 This project requires two short animations. If you don't like mine you can make your own.
 
 You'll need [python](https://www.python.org/about/gettingstarted/) & [FFmpeg](https://ffmpeg.org/) installed on your computer.
@@ -53,9 +48,8 @@ You'll need [python](https://www.python.org/about/gettingstarted/) & [FFmpeg](ht
 - Export the two source videos you'd like to turn into animations. Use a low resolution as you'll eventually be resizing each frame to 9px x 16px.
 - Place each video in their own folders named **flame-normal-source-mp4** and **flame-flicker-source-mp4**
 - Name the normal flame video `flame-normal-source.mp4`, and name the flicker flame video `flame-flicker-source.mp4`
-- We're going to use `FFmpeg` via the command line to create PNGs. 
-- We'll start with the flicker video. Navigate into the **flame-flicker-source-mp4** folder: 
-
+- We're going to use `FFmpeg` via the command line to create PNGs.
+- We'll start with the flicker video. Navigate into the **flame-flicker-source-mp4** folder:
 
 ```
 cd flame-flicker-source-mp4
@@ -66,7 +60,6 @@ cd flame-flicker-source-mp4
 ```
  ffmpeg -i flame-flicker-source.mp4 -vf "fps=30,scale=9:16" flame-flicker-source-pngs/flicker-frame_%03d.png
 ```
-
 
 - Navigate into the **flame-flicker-source-pngs** folder to confirm you see a bunch of PNGs that are 9x16.
 
@@ -82,7 +75,6 @@ python3 convert-flame-flicker.py *.png > data-flame-flicker.h
 
 - Move the `h` file `data-flame-flicker.h` into the root of the project folder: led-candle.
 
-
 - Repeat the above steps for the normal video **flame-normal-source.mp4**.
 
 ```
@@ -94,9 +86,11 @@ python3 convert-flame-flicker.py *.png > data-flame-flicker.h
 ```
 cd flame-normal-source-pngs
 ```
+
 ```
 python3 convert-flame-normal.py *.png > data-flame-normal.h
 ```
+
 - Move the `h` file `data-flame-flicker.h` into the root of the project folder: led-candle.
 
 - Confirm that the project folder contains the three files needed to upload to the Adafruit Pro Trinket: `led-candle.ino`, `data-flame-normal.h`, `data-flame-flicker.h`.
@@ -105,14 +99,14 @@ python3 convert-flame-normal.py *.png > data-flame-normal.h
 
 - Using the Arduino IDE, open `led-candle.ino`.
 
-- Upload `led-candle.ino`, `data-flame-normal.h` & `data-flame-flicker.h` to the Pro Trinket board by pressing the tiny button on the Pro Trinket. Within eight seconds, select Sketch > Upload Using Programmer. Wait until you see the message "Done uploading". 
-
+- Upload `led-candle.ino`, `data-flame-normal.h` & `data-flame-flicker.h` to the Pro Trinket board by pressing the tiny button on the Pro Trinket. Within eight seconds, select Sketch > Upload Using Programmer. Wait until you see the message "Done uploading".
 
 ## Parts
-- Pro Trinket microcontroller [2010](https://www.adafruit.com/product/2010) or [2000](https://www.adafruit.com/product/2000)) (2010 = 3V/12MHz for longest battery life, but 5V/16MHz works OK)
+
+- Adafruit ItsyBitsy MO Express microcontroller [3727](https://www.adafruit.com/product/3727)
 - Charlieplex LED Matrix Driver (Adafruit product id: [2946](https://www.adafruit.com/product/2946))
 - Charlieplex LED Matrix (Adafruit product id: 2947, [2948](https://www.adafruit.com/product/2948), 2972, 2973 or 2974)
 - 350 mAh LiPoly battery (Adafruit product id: [2750](https://www.adafruit.com/product/2750))
 - LiPoly backpack (Adafruit product id: [2124](https://www.adafruit.com/product/2224))
 - On-Off Power Button / Pushbutton Toggle Switch (Adafruit product id: [1683](https://www.adafruit.com/product/1683))
-- Ultrasonic Distance Sensor (Adafruit product id: [4007](https://www.adafruit.com/product/4007))
+- Adafruit VL53LOX Time of Flight Distance Sensor (Adafruit product id: [3317](https://www.adafruit.com/product/3317))
--- led-candle.ino
+++ led-candle.ino
@@ -1,51 +1,27 @@
-// SPDX-FileCopyrightText: 2019 Phillip Burgess/paintyourdragon for Adafruit Industries
-//
-// SPDX-License-Identifier: MIT
+#include <Wire.h>           // For I2C communication
+#include <Adafruit_VL53L0X.h>
+#include <Adafruit_IS31FL3731.h>
+#include "animationFlicker.h"      // Flame animation data
+#include "animationNormal.h"       // Import the data-flame-normal file
 
-//--------------------------------------------------------------------------
-// Animated flame for Adafruit Pro Trinket.  Uses the following parts:
-//   - Pro Trinket microcontroller (adafruit.com/product/2010 or 2000)
-//     (#2010 = 3V/12MHz for longest battery life, but 5V/16MHz works OK)
-//   - Charlieplex LED Matrix Driver (2946)
-//   - Charlieplex LED Matrix (2947, 2948, 2972, 2973 or 2974)
-//   - 350 mAh LiPoly battery (2750)
-//   - LiPoly backpack (2124)
-//   - SPDT Slide Switch (805)
-//--------------------------------------------------------------------------
+#define LED_DRIVER_ADDR 0x74       // I2C address of Charlieplex matrix
+#define SDA_PIN 0
+#define SCL_PIN 1
 
-#include <Wire.h>                // Library for I2C communication
-#include "data-flame-normal.h"   // Flame animation-normal data
-#include "data-flame-flicker.h"  // Flame animation-flicker data
-#include <avr/power.h>           // Peripheral control and
-#include <avr/sleep.h>           // sleep to minimize current draw
+// Instantiate objects
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+Adafruit_IS31FL3731 ledmatrix = Adafruit_IS31FL3731();
 
-#define I2C_ADDR 0x74            // I2C address of Charlieplex matrix
-
-uint8_t page = 0;                         // Front/back buffer control
-char animation = 0;                       // We're only using this as indicator. 0 means normal, 1 means flicker
-uint8_t *ptr  = animationNormal;          // Current pointer into normal animation data
-uint8_t img[9 * 16];                      // Buffer for rendering image
-
-
-// The distance sensor
-// Hook up HC-SR04 with Trig to Arduino Pin 10, Echo to Arduino pin 11
-#define trigPin 10 
-#define echoPin 11 
-
-// Variables that will track the response from the HC-SR04 Echo Pin
-float duration;   
-float distance;
+uint8_t page = 0;    // Front/back buffer control
+char animation = 0;     // We're only using this as an indicator. 0 means animationNormal, 1 means animationFlicker
+const uint8_t *ptr  = animationFlicker; // Current pointer into animation data
+uint8_t  img[9 * 16]; // Buffer for rendering image
 
 // UTILITY FUNCTIONS -------------------------------------------------------
 
-// The full IS31FL3731 library is NOT used by this code. Instead, 'raw'
-// writes are made to the matrix driver.  This is to maximize the space
-// available for animation data.  Use the Adafruit_IS31FL3731 and
-// Adafruit_GFX libraries if you need to do actual graphics stuff.
-
 // Begin I2C transmission and write register address (data then follows)
 void writeRegister(uint8_t n) {
-  Wire.beginTransmission(I2C_ADDR);
+  Wire.beginTransmission(LED_DRIVER_ADDR);
   Wire.write(n);
   // Transmission is left open for additional writes
 }
@@ -58,25 +34,15 @@ void pageSelect(uint8_t n) {
 }
 
 // SETUP FUNCTION - RUNS ONCE AT STARTUP -----------------------------------
+
 void setup() {
-
-// distance sensor setup
-  Serial.begin (9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
-// led animation setup
   uint8_t i, p, byteCounter;
-  power_all_disable(); // Stop peripherals: ADC, timers, etc. to save power
-  power_twi_enable();  // But switch I2C back on; need it for display
-  DIDR0 = 0x0F;        // Digital input disable on A0-A3
 
   // The Arduino Wire library runs I2C at 100 KHz by default.
   // IS31FL3731 can run at 400 KHz.  To ensure fast animation,
   // override the I2C speed settings after init...
   Wire.begin();                            // Initialize I2C
-  TWSR = 0;                                // I2C prescaler = 1
-  TWBR = (F_CPU / 400000 - 16) / 2;        // 400 KHz I2C
+  Wire.setClock(400000);                   // 400 KHz I2C
   // The TWSR/TWBR lines are AVR-specific and won't work on other MCUs.
 
   pageSelect(0x0B);                        // Access the Function Registers
@@ -86,8 +52,8 @@ void setup() {
   for(p=0; p<2; p++) {                     // For each page used (0 & 1)...
     pageSelect(p);                         // Access the Frame Registers
     writeRegister(0);                      // Start from 1st LED control reg
-    for(i=0; i<18; i++) Wire.write(0xFF);  // Enable all LEDs (18*8=144) NOTE: isn't this 16*9?
-    for(byteCounter = i+1; i<0xB4; i++) {  // For blink & brightness(PWM) registers...
+    for(i=0; i<18; i++) Wire.write(0xFF);  // Enable all LEDs (18*8=144)
+    for(byteCounter = i+1; i<0xB4; i++) {  // For blink & PWM registers...
       Wire.write(0);                       // Clear all
       if(++byteCounter >= 32) {            // Every 32 bytes...
         byteCounter = 1;                   // End I2C transmission and
@@ -102,161 +68,156 @@ void setup() {
   // This provides a sufficiently steady time reference for animation,
   // allows timer/counter peripherals to remain off (for power saving)
   // and can power-down the chip after processing each frame.
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Deepest sleep mode (WDT wakes)
-  noInterrupts();
-  MCUSR  &= ~_BV(WDRF);
-  WDTCSR  =  _BV(WDCE) | _BV(WDE);     // WDT change enable
-  WDTCSR  =  _BV(WDIE) | _BV(WDP0);    // Interrupt enable, ~32 ms
-  interrupts();
+  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
   // Peripheral and sleep savings only amount to about 10 mA, but this
   // may provide nearly an extra hour of run time before battery depletes.
+
+
+// **************************************
+// ******** TimeofFlight setup **********
+// **************************************
+    // Setup serial communication
+  Serial.begin(115200);
+
+  // Setup I2C
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  // Start the VL53L0X sensor
+  if (!lox.begin()) {
+    Serial.println(F("Failed to initialize the VL53L0X sensor"));
+    while (1);
+  }
+
+  // Initialize the LED matrix
+  if (!ledmatrix.begin(LED_DRIVER_ADDR)) {
+    Serial.println(F("Failed to initialize the LED matrix"));
+    while (1);
+  }
+
+  ledmatrix.clear(); // Clear the LED matrix buffer
+// **************************************
+// **************************************
+
+
+
 }
 
+// LOOP FUNCTION - RUNS EVERY FRAME ----------------------------------------
+
 void loop() {
-  // ==========================================================
-  // ============== Ultrasonic Distance Sensor ================
-  // ==========================================================
-  // Write a pulse to the HC-SR04 Trigger Pin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  // Measure the response from the HC-SR04 Echo Pin
-  duration = pulseIn(echoPin, HIGH);
-  
-  // Determine distance from duration
-  // Use 343 metres per second as speed of sound
-  distance = (duration / 2) * 0.0343;
-  // ==========================================================
-  
-  // If distance is equal to or less than 30 cm show animationFlicker
-  if (distance <= 30) {
-      digitalWrite(LED_BUILTIN, HIGH);    // Tiny red LED on for testing
-   
-   if (animation == 0) {
-      animation = 1;
-      ptr = animationFlicker;
-   }
-   
-    uint8_t  a, x1, y1, x2, y2, x, y;
-    
-    
-    power_twi_enable();
-    // Datasheet recommends that I2C should be re-initialized after enable,
-    // but Wire.begin() is slow.  Seems to work OK without.
 
-    // Display frame rendered on prior pass.  This is done at function start
-    // (rather than after rendering) to ensire more uniform animation timing.
-    pageSelect(0x0B);    // Function registers
-    writeRegister(0x01); // Picture Display reg
-    Wire.write(page);    // Page #
-    Wire.endTransmission();
-    
-    page ^= 1; // Flip front/back buffer index
+  VL53L0X_RangingMeasurementData_t measure;
+  lox.rangingTest(&measure, false); // Get a ranging measurement
 
-    // Then render NEXT frame.  Start by getting bounding rect for new frame:
-    a = pgm_read_byte(ptr++);     // New frame X1/Y1
-    // a = pgm_read_byte(ptrFlicker++);     // New frame X1/Y1
+  if (measure.RangeStatus != 4) { // Check if the measurement is valid
+    uint16_t distance = measure.RangeMilliMeter;
 
-    if(a >= 0x90) {               // End of Data(EOD) marker? (valid X1 never exceeds 8)
-      ptr = animationFlicker;     // Reset animation data pointer to start
-      a   = pgm_read_byte(ptr++); // and take first value
-    }
-    x1 = a >> 4;                  // X1 = high 4 bits
-    y1 = a & 0x0F;                // Y1 = low 4 bits
-    a  = pgm_read_byte(ptr++);    // New frame X2/Y2
-    x2 = a >> 4;                  // X2 = high 4 bits
-    y2 = a & 0x0F;                // Y2 = low 4 bits
 
-    // Read rectangle of data from anim[] into portion of img[] buffer
-    for(x=x1; x<=x2; x++) { // Column-major
-      for(y=y1; y<=y2; y++) img[(x << 4) + y] = pgm_read_byte(ptr++);
-    }
+    // #########################################
+    // Distance is less than or equal to 30 cm
+    // insert animationFlicker
+    // #########################################
+    if (distance <= 300) {
+      digitalWrite(LED_BUILTIN, HIGH);   // Tiny red LED on for testing
 
-    // Write img[] to matrix (not actually displayed until next pass)
-    pageSelect(page);    // Select background buffer
-    writeRegister(0x24); // First byte of brightness(PWM) data
-    uint8_t i = 0, byteCounter = 1;
-    for(uint8_t x=0; x<9; x++) {
-      for(uint8_t y=0; y<16; y++) {
-        Wire.write(img[i++]);      // Write each byte to matrix
-        if(++byteCounter >= 32) {  // Every 32 bytes...
-          Wire.endTransmission();  // end transmission and
-          writeRegister(0x24 + i); // start a new one (Wire lib limits)
-        }
+      if (animation == 0) {
+        animation = 1;
+        ptr = animationFlicker;
       }
-    }
-    delayMicroseconds(5000);
-    Wire.endTransmission();
 
-    power_twi_disable(); // I2C off (see comment at top of function)
-    sleep_enable();
-    interrupts();
-    sleep_mode();        // Power-down MCU.
-    // Code will resume here on wake; loop() returns and is called again
-    // Interrupt the regular animation right away
-    // Conditional statement to determine if first loop or not?
+        uint8_t  a, x1, y1, x2, y2, x, y;
+        // Display frame rendered on prior pass.  This is done at function start
+        // (rather than after rendering) to ensire more uniform animation timing.
+        pageSelect(0x0B);    // Function registers
+        writeRegister(0x01); // Picture Display reg
+        Wire.write(page);    // Page #
+        Wire.endTransmission();
 
-  // If distance is more than 30 cm show aranimationNormal
- } else { 
-    digitalWrite(LED_BUILTIN, LOW);   // Tiny red LED off for testing
-    animation = 0;
-    uint8_t  a, x1, y1, x2, y2, x, y;
+        page ^= 1; // Flip front/back buffer index
 
-    power_twi_enable();
-    // Datasheet recommends that I2C should be re-initialized after enable,
-    // but Wire.begin() is slow.  Seems to work OK without.
-
-    // Display frame rendered on prior pass.  This is done at function start
-    // (rather than after rendering) to ensire more uniform animation timing.
-    pageSelect(0x0B);    // Function registers
-    writeRegister(0x01); // Picture Display reg
-    Wire.write(page);    // Page #
-    Wire.endTransmission();
-
-    page ^= 1; // Flip front/back buffer index
-
-    // Then render NEXT frame.  Start by getting bounding rect for new frame:
-    a = pgm_read_byte(ptr++);     // New frame X1/Y1
-    if(a >= 0x90) {               // EOD marker? (valid X1 never exceeds 8)
-      ptr = animationNormal;     // Reset animation data pointer to start
-      a   = pgm_read_byte(ptr++); // and take first value
-    }
-    x1 = a >> 4;                  // X1 = high 4 bits 
-    y1 = a & 0x0F;                // Y1 = low 4 bits
-    a  = pgm_read_byte(ptr++);    // New frame X2/Y2
-    x2 = a >> 4;                  // X2 = high 4 bits
-    y2 = a & 0x0F;                // Y2 = low 4 bits
-
-    // Read rectangle of data from anim[] into portion of img[] buffer
-    for(x=x1; x<=x2; x++) { // Column-major
-      for(y=y1; y<=y2; y++) img[(x << 4) + y] = pgm_read_byte(ptr++);
-    }
-
-    // Write img[] to matrix (not actually displayed until next pass)
-    pageSelect(page);    // Select background buffer
-    writeRegister(0x24); // First byte of brightness(PWM) data
-    uint8_t i = 0, byteCounter = 1;
-    for(uint8_t x=0; x<9; x++) {
-      for(uint8_t y=0; y<16; y++) {
-        Wire.write(img[i++]);      // Write each byte to matrix
-        if(++byteCounter >= 32) {  // Every 32 bytes...
-          Wire.endTransmission();  // end transmission and
-          writeRegister(0x24 + i); // start a new one (Wire lib limits)
+        // Then render NEXT frame.  Start by getting bounding rect for new frame:
+        a = pgm_read_byte(ptr++);     // New frame X1/Y1
+        if(a >= 0x90) {               // EOD marker? (valid X1 never exceeds 8)
+          ptr = animationFlicker;     // Reset animation data pointer to start
+          a   = pgm_read_byte(ptr++); // and take first value
         }
-      }
-    }
-    Wire.endTransmission();
+        x1 = a >> 4;                  // X1 = high 4 bits
+        y1 = a & 0x0F;                // Y1 = low 4 bits
+        a  = pgm_read_byte(ptr++);    // New frame X2/Y2
+        x2 = a >> 4;                  // X2 = high 4 bits
+        y2 = a & 0x0F;                // Y2 = low 4 bits
 
-    power_twi_disable(); // I2C off (see comment at top of function)
-    sleep_enable();
-    interrupts();
-    sleep_mode();        // Power-down MCU.
-    // Code will resume here on wake; loop() returns and is called again
+        // Read rectangle of data from anim[] into portion of img[] buffer
+        for(x=x1; x<=x2; x++) { // Column-major
+          for(y=y1; y<=y2; y++) img[(x << 4) + y] = pgm_read_byte(ptr++);
+        }
+
+        // Write img[] to matrix (not actually displayed until next pass)
+        pageSelect(page);    // Select background buffer
+        writeRegister(0x24); // First byte of PWM data
+        uint8_t i = 0, byteCounter = 1;
+        for(uint8_t x=0; x<9; x++) {
+          for(uint8_t y=0; y<16; y++) {
+            Wire.write(img[i++]);      // Write each byte to matrix
+            if(++byteCounter >= 32) {  // Every 32 bytes...
+              Wire.endTransmission();  // end transmission and
+              writeRegister(0x24 + i); // start a new one (Wire lib limits)
+            }
+          }
+        }
+        Wire.endTransmission();
+
+      
+    // #########################################
+    // insert animationNormal
+    // Distance is more than 30 cm
+    // #########################################
+    } else {
+      digitalWrite(LED_BUILTIN, LOW);   // Tiny red LED off for testing
+
+        animation = 0;
+        uint8_t  a, x1, y1, x2, y2, x, y;
+        // Display frame rendered on prior pass.  This is done at function start
+        // (rather than after rendering) to ensire more uniform animation timing.
+        pageSelect(0x0B);    // Function registers
+        writeRegister(0x01); // Picture Display reg
+        Wire.write(page);    // Page #
+        Wire.endTransmission();
+
+        page ^= 1; // Flip front/back buffer index
+
+        // Then render NEXT frame.  Start by getting bounding rect for new frame:
+        a = pgm_read_byte(ptr++);     // New frame X1/Y1
+        if(a >= 0x90) {               // EOD marker? (valid X1 never exceeds 8)
+          ptr = animationNormal;     // Reset animation data pointer to start
+          a   = pgm_read_byte(ptr++); // and take first value
+        }
+        x1 = a >> 4;                  // X1 = high 4 bits
+        y1 = a & 0x0F;                // Y1 = low 4 bits
+        a  = pgm_read_byte(ptr++);    // New frame X2/Y2
+        x2 = a >> 4;                  // X2 = high 4 bits
+        y2 = a & 0x0F;                // Y2 = low 4 bits
+
+        // Read rectangle of data from anim[] into portion of img[] buffer
+        for(x=x1; x<=x2; x++) { // Column-major
+          for(y=y1; y<=y2; y++) img[(x << 4) + y] = pgm_read_byte(ptr++);
+        }
+
+        // Write img[] to matrix (not actually displayed until next pass)
+        pageSelect(page);    // Select background buffer
+        writeRegister(0x24); // First byte of PWM data
+        uint8_t i = 0, byteCounter = 1;
+        for(uint8_t x=0; x<9; x++) {
+          for(uint8_t y=0; y<16; y++) {
+            Wire.write(img[i++]);      // Write each byte to matrix
+            if(++byteCounter >= 32) {  // Every 32 bytes...
+              Wire.endTransmission();  // end transmission and
+              writeRegister(0x24 + i); // start a new one (Wire lib limits)
+            }
+          }
+        }
+        Wire.endTransmission();
+    }
   }
 
 }
-
-ISR(WDT_vect) { } // Watchdog timer interrupt (does nothing, but required)
